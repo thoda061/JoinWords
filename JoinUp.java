77ec7fa8d86dd5a824bad7f5eb64ef8a59bf3e92
will-shaw <will_shaw@live.com>
2017-10-10 20:48:37 +1300

    Tweaks and formatting.

--- JoinUp.java
+++ JoinUp.java
@@ -7,7 +7,7 @@ import java.util.*;
  *
  * @author Daniel Thomson, ID: 5040702 & Will Shaw, ID: 8291780
  */
-public class JoinUp{
+public class JoinUp {
 
     //First word in the sequence
     private static String firstWord;
@@ -23,14 +23,14 @@ public class JoinUp{
      *
      * @param args command line arguments usd to set first and last word.
      */
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         firstWord = args[0];
         lastWord = args[1];
 
         // If the words are the same, then we immediately know they are not
         // singly joined, and the shortest double join is it and itself.
         if (firstWord.equals(lastWord)) {
-            System.out.println("1 " + firstWord);            
+            System.out.println("1 " + firstWord);
             System.out.println("1 " + firstWord);
             System.exit(0);
         }
@@ -40,8 +40,8 @@ public class JoinUp{
         //letter and storing all the ArrayLists in a HashMap.
         while (sc.hasNextLine()) {
             String word = sc.nextLine();
-            if(dict.containsKey(word.charAt(0))) {
-                 dict.get(word.charAt(0)).add(word);
+            if (dict.containsKey(word.charAt(0))) {
+                dict.get(word.charAt(0)).add(word);
             } else {
                 ArrayList<String> wordGroup = new ArrayList<>();
                 wordGroup.add(word);
@@ -56,7 +56,7 @@ public class JoinUp{
      * Finds sequences of words that are either doubly linked or singly linked
      * between firstWord and lastWord, then prints them.
      */
-    public static void findSequence () {
+    public static void findSequence() {
         //Queue for constructing sequences in a breadth first manner
         LinkedList<LinkedWord> q;
         //Current word we are looking at
@@ -73,7 +73,7 @@ public class JoinUp{
 
         //For loop for finding two sequence, once for singly joined and once
         // for doubly joined.
-        for(int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) {
             //Resets variables for each sequence
             q = new LinkedList<>();
             usedWords = new HashSet<>();
@@ -84,11 +84,11 @@ public class JoinUp{
 
             //While queue has items and sequence length is less the dictionary
             // size (to stop potential infinte loops of joined words)
-            while(q.peek() != null && currDepth != dict.size()) {
+            while (q.peek() != null && currDepth != dict.size()) {
                 currWord = q.remove();
                 currDepth = currWord.getDepth();
                 //Check if currWord is joined with lastWord
-                if(joinWords(currWord.getWord(), lastWord, i)) {
+                if (joinWords(currWord.getWord(), lastWord, i)) {
                     //If joined, add words in sequence to output, by using the
                     //parent variable of currWord to traverse the sequence
                     //backwards
@@ -107,38 +107,38 @@ public class JoinUp{
 
                     //Gets all unique chars of currWord
                     HashSet<Character> wordChars = new HashSet<>();
-                    for(char c: currWord.getWord().toCharArray()) {
+                    for (char c : currWord.getWord().toCharArray()) {
                         wordChars.add(c);
                     }
                     //Use chars of currWord to select specfic word groups from
                     //dict
-                    for(Character c: wordChars) {
-                       if(dict.containsKey(c)) {
-                          ArrayList<String> wordGroup = dict.get(c);
-                          //Tries to join each word in word group with
-                          //currWord, if word not in usedWords
-                          for(String word: wordGroup) {
-                             if(!usedWords.contains(word)) {
-                                if(joinWords(currWord.getWord(), word, i)) {
-                                   //Add word to usedWords
-                                   usedWords.add(word);
-                                   LinkedWord newLink = new LinkedWord(word);
-                                   //Set currWord as parent of new words, so we
-                                   // can trace the sequence when outputing
-                                   newLink.setParent(currWord);
-                                   //Set new words depth to one more than
-                                   //currWord
-                                   newLink.setDepth(currWord.getDepth() + 1);
-                                   q.add(newLink);
+                    for (Character c : wordChars) {
+                        if (dict.containsKey(c)) {
+                            ArrayList<String> wordGroup = dict.get(c);
+                            //Tries to join each word in word group with
+                            //currWord, if word not in usedWords
+                            for (String word : wordGroup) {
+                                if (!usedWords.contains(word)) {
+                                    if (joinWords(currWord.getWord(), word, i)) {
+                                        //Add word to usedWords
+                                        usedWords.add(word);
+                                        LinkedWord newLink = new LinkedWord(word);
+                                        //Set currWord as parent of new words, so we
+                                        // can trace the sequence when outputing
+                                        newLink.setParent(currWord);
+                                        //Set new words depth to one more than
+                                        //currWord
+                                        newLink.setDepth(currWord.getDepth() + 1);
+                                        q.add(newLink);
+                                    }
                                 }
-                             }         
-                          }
-                       }
+                            }
+                        }
                     }
                 }
             }
             //if sequence was not found output 0
-            if(!found) {
+            if (!found) {
                 System.out.println("0");
             }
         }
@@ -153,36 +153,35 @@ public class JoinUp{
      * @return boolean true if join possible between given words, false other
      *                  wise.
      */
-    public static boolean joinWords(String first,String second,int joinType) {
-        int length = first.length() > second.length() ? second.length() :
-            first.length();
+    public static boolean joinWords(String first, String second, int joinType) {
+        int length = first.length() > second.length() ? 
+                                      second.length() : first.length();
         int match = length + 1;
 
-        while(match > length/2) {
+        while (match > length / 2) {
             String suf = first.substring(first.length() - (match - 1));
             String pre = second.substring(0, match - 1);
 
-            if(suf.equals(pre)) {
+            if (suf.equals(pre)) {
                 break;
             }
             match--;
         }
 
-        if(joinType == 0) {
-            if(match > first.length()/2 &&
-               match > second.length()/2) {
+        if (joinType == 0) {
+            if (match > first.length() / 2 && match > second.length() / 2) {
                 return true;
             }
         }
 
-        if(joinType == 1) {
-            if(match > first.length()/2 + first.length()%2 &&
-               match > second.length()/2 + second.length()%2) {
+        if (joinType == 1) {
+            if (match > first.length() / 2 + first.length() % 2 
+                && match > second.length() / 2 + second.length() % 2) {
                 return true;
             }
         }
-        
+
         return false;
     }
-        
+
 }
